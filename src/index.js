d5d361c8ce98a5a9c2d65cf8e50eac01aecebc7d
Chen-Private <chenasulin25@gmail.com>
2023-03-13 20:10:55 +0200

    test3

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ const express = require('express');
 const port = process.env.port || 3000;
 const app = express();
 let x = 0;
+
 app.get('/', (req, res) => {
   x += 1;
+  console.log('a blank request has arrived');
   res.send(`<h1>Hellow World ${x}</h1>`);
 });
 
-app.get('/', (req, res) => {
-  console.log('a blank request has arrived');
-  res.send('<h1>Hellow World 3</h1>');
-});
 
 app.listen(port, () => {
   console.log(`Server is up and running on http://127.0.0.1:${port}`);
